bb4c3d0aac107792d02dad1796d67f49727bed61
goqflash <goqflash@gmail.com>
2015-09-28 13:42:55 +0400

    Demo version

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+get_header();
+?>
+<div class="content_header">
+
+    <div id="inner_content_header" class="container ">
+        <div class="i_left col-md-10">
+            <h1>Benefits to Expect from IT Service Providers in Dubai</h1>
+        </div>
+        <div class="i_right col-md-2">
+            <a href="#" class="back_to_blog">Back to blog</a>
+        </div>
+    </div>
+</div>
+<div class="container">
+</div>
+<?php
+get_footer();
+?>
---
+++ page-home.php
@@ -0,0 +1,11 @@
+<?php
+get_header();
+?>
+
+<div id="featured_page_content" class="container featured_page_content">
+    <?php get_template_part( 'template-parts/featured-area' ); ?>
+</div>
+
+<?php
+get_footer();
+?>
